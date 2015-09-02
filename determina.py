57bb14cd0497e53556ccf6a87a7d11f10ac4f6fb
lucasmpalma <lucaspalma.m@gmail.com>
2015-09-02 20:27:58 -0300

    anything

--- determina.py
+++ determina.py
@@ -5,10 +5,17 @@ q2 = {0: ['M'], 1: ['q3']}
 q3 = {0: ['M'], 1: ['M']}
 M = {0: ['M'], 1: ['M']}
 s = {'q0': q0, 'q1': q1, 'q2': q2, 'q3': q3, 'M': M}
+# --------------------------------------------------
+q4 = {1: ['q4'], 2: ['M2'], 3: ['M2'], '&': ['q5']}
+q5 = {1: ['M2'], 2: ['q5'], 3: ['M2'], '&': ['q6']}
+q6 = {1: ['M2'], 2: ['M2'], 3: ['q6'], '&': ['M2']}
+M2 = {1: ['M2'], 2: ['M2'], 3: ['M2'], '&': ['M2']}
+se = {'q4': q4, 'q5': q5, 'q6': q6, 'M2': M2}
+#---------------------------------------------------
+
 d = s.copy()
 st = []
 alfabeto = q0.keys()
-fim = False
 
 for key, value in s.items():
     if key not in st:
@@ -50,19 +57,37 @@ def procuraEstados(s):
                 if newState not in st and newState != '':
                     newState = ''
                     criaEstado(v)
-def calculaFecho():
-     sFecho = {}
-     for key, value in s.items():
-        
 
-def determina (s):
+def calculaFecho():
+    sFecho = {}
+    Ytransition = []
+    for key, value in se.items():
+        if key !='M2':
+            sFecho[key] = []
+    for key, value in se.items():
+        for k, v in value.items():
+            for item in v:
+                if k == '&' and key != 'M2' and item != 'M2':
+                    sFecho[key].append(item)
+    for key, value in sFecho.items():
+        for item in value:
+            aux = sFecho[item]
+            for it in aux:
+                value.append(it)
+    for key, value in sFecho.items():
+        sFecho[key].append(key)                     
+    return sFecho 
+
+
+def determina(s):
     if '&' in alfabeto:
-        print("Automato com & transições")
+        print("Automato com & transicoes")
     else:   
         procuraEstados(s)
         while len(s) != len(d):
             atualizaEstados(d)
             procuraEstados(s)
         print(s)
-        #asdfasdf
-determina(s)
+
+test = calculaFecho()
+print(test)
