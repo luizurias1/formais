3250c6e07a166c6e1873fe10f038b71bf57d3449
luizurias1 <luiz.urias@grad.ufsc.br>
2015-09-08 15:29:22 -0300

    grammar to automata add

---
+++ grammar.py
@@ -0,0 +1,65 @@
+__author__ = 'luizu'
+
+terminal = ['a', 'b']
+nTerminal = ['A', 'B', 'S']
+producoes = {
+    'S': ['aA', 'bB', 'a', 'b'],
+    'A': ['aA', 'bA', 'a'],
+    'B': ['bB', 'aB', 'b']
+}
+
+class Grammar:
+
+    def __init__(self,producoes,terminais,nonTerminais):
+        self.producoes = producoes
+        self.terminais = terminais
+        self.nonTerminais = nonTerminais
+
+    def initEstados(self):
+        estadosAutomato = []
+        for i in self.nonTerminais:
+            estadosAutomato.append(i)
+
+        estadosAutomato.append('F')
+        estadosAutomato.append('M')
+
+        s = {}
+        aux = {}
+        test = {}
+
+        for a in estadosAutomato:
+            s[a] = aux
+        return s
+
+    def convertGtoAF(self):
+        s = {}
+        s = self.initEstados()
+        transicao = ''
+        estado = ''
+
+        for key, value in self.producoes.items():
+            test = {}
+            for item in value:
+                if item in self.terminais:
+                    test[item].append('F')
+                else:
+                  for caracter in item:
+                      if caracter.islower():
+                          transicao = caracter
+                      if caracter.isupper():
+                          estado = caracter
+                      test[transicao] = [estado]
+            s[key] = test
+
+        for key, value in s.items():
+            if key == 'M' or  key == 'F':
+                for item in terminal:
+                    test[item] = ['M']
+                s[key] = test
+        return s
+
+#########################################################################
+
+g = Grammar(producoes,terminal,nTerminal)
+automato = g.convertGtoAF()
+print(automato)
