aed8ddaf7dde73fa891fa1dcdeaa4587f8549de2
luizurias1 <luiz.urias@grad.ufsc.br>
2015-09-08 21:02:08 -0300

    refatorado para python 3

--- leitor.py
+++ leitor.py
@@ -2,65 +2,65 @@ import json
 
 class Leitor :
 
-	def __init__(self):
-		self.rejected = ['U', 'E', 'K', 'S', ' ', '=', '{', '}', ',', '(', ')', '[', ']', '>']
-		pass
+    def __init__(self):
+        self.rejected = ['U', 'E', 'K', 'S', ' ', '=', '{', '}', ',', '(', ')', '[', ']', '>']
+        pass
 
-	def ler(self):
-		file = open('entrada', 'r')
-		dict = {}
-		transition = []
-		for line in file:
-			for world in line:
-				if world == 'K':
-					dict = self.geraEstados(line)
-				if world == 'E':
-					dict, transition = self.geraAlfabeto(line, dict)
-				if world == 'S':
-					self.completaAutomato(dict, line, transition)
-		return dict 
+    def ler(self):
+        file = open('entrada', 'r')
+        dict = {}
+        transition = []
+        for line in file:
+            for world in line:
+                if world == 'K':
+                    dict = self.geraEstados(line)
+                if world == 'E':
+                    dict, transition = self.geraAlfabeto(line, dict)
+                if world == 'S':
+                    self.completaAutomato(dict, line, transition)
+        return dict
 
-	def completaAutomato(self, dict, line, transition):
-		fileName = ''
-		for element in line:
-			if element == '\n':
-				with open(fileName) as data_file:    
-   	 				data = json.load(data_file)
-   	 				for key, value in dict.items():
-   	 					for k, v in value.items():
-   	 						tamanho = len(data[key][k])
-							for x in xrange(0,tamanho):
-   	 							a = data[key][k][x]
-   	 							v.append(a)
-			if element not in self.rejected:
-				fileName += element
-	
-	def geraTransicoes(self, line, dict):
-		state = ''
-		for element in line:
-			if element not in self.rejected:
-				state += element
+    def completaAutomato(self, dict, line, transition):
+        fileName = ''
+        for element in line:
+            if element == '\n':
+                with open(fileName) as data_file:
+                    data = json.load(data_file)
+                    for key, value in dict.items():
+                        for k, v in value.items():
+                            tamanho = len(data[key][k])
+                            for x in range(0,tamanho):
+                                a = data[key][k][x]
+                                v.append(a)
+            if element not in self.rejected:
+                fileName += element
 
-	def geraAlfabeto(self, line, dict):
-		transition = ''
-		t = []
-		for element in line:
-			if element == ',' or element == ')':
-				for key, value in dict.items():
-					value[transition] = []
-				transition = ''
-			if element not in self.rejected:
-				transition += element
-			t.append(transition)
-		return dict, t 
+    def geraTransicoes(self, line, dict):
+        state = ''
+        for element in line:
+            if element not in self.rejected:
+                state += element
 
-	def geraEstados(self, line):
-		state = ''
-		dict = {}
-		for element in line:
-			if element == ',':
-				dict[state] = {}
-				state = ''
-			if element not in self.rejected:
-				state += element
-		return dict
+    def geraAlfabeto(self, line, dict):
+        transition = ''
+        t = []
+        for element in line:
+            if element == ',' or element == ')':
+                for key, value in dict.items():
+                    value[transition] = []
+                transition = ''
+            if element not in self.rejected:
+                transition += element
+            t.append(transition)
+        return dict, t
+
+    def geraEstados(self, line):
+        state = ''
+        dict = {}
+        for element in line:
+            if element == ',':
+                dict[state] = {}
+                state = ''
+            if element not in self.rejected:
+                state += element
+        return dict
